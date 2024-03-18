8c015f97796d5cf025cc502ac40e976f0d6d98ae
pakkkek <kpojlukkreativnyj@gmail.com>
2024-03-18 17:48:53 +0100

    HW_11.03.24 was done.

--- src/main.ts
+++ src/main.ts
@@ -6,28 +6,34 @@ import 'zone.js';
   selector: 'app-root',
   standalone: true,
   template: `
-    <h1>Hello from {{ name }}!</h1>
-    <p>{{ monologue }}</p>
-    <h2>Photo Gallery</h2>
-    <div *ngFor="let photo of photos">
-      <img [src]="photo" alt="Photo">
-    </div>
+  <div>
+  <h2>Цитати Білла Гейтса</h2>
+  <ul>
+    <li *ngFor="let quote of quotes">{{ quote }}</li>
+  </ul>
+  <h2>Біографія</h2>
+  <p>{{ bio }}</p>
+  <img src="./images/bill-gates.jpg" alt="Портрет Білла Гейтса">
+  <h2>Додаткові ресурси</h2>
+  <ul>
+    <li><a href="https://en.wikipedia.org/wiki/Bill_Gates">Wikipedia</a></li>
+    <li><a href="https://www.gatesfoundation.org/">Bill & Melinda Gates Foundation</a></li>
+    <li><a href="https://www.gatesnotes.com/">Gates Notes</a></li>
+    <li><a href="https://twitter.com/billgates">Twitter</a></li>
+  </ul>
+  </div>  
   `,
 })
 export class App {
-  name = 'Angular';
-  monologue: string = `
-    To be, or not to be, that is the question:
-    Whether 'tis nobler in the mind to suffer
-    The slings and arrows of outrageous fortune,
-    Or to take arms against a sea of troubles
-    And, by opposing, end them.
-  `;
-  photos: string[] = [
-    'assets/images/car.jpg',
-    'assets/images/logo.jpg',
-    'assets/images/founder.jpg',
+  quotes: string[] = [
+    "Patience is a key element of success",
+    "If you think your teacher is tough, wait till you get boss",
+    "Life is not fair — get used to it!",
+    "Success is a lousy teacher. It seduces smart people into thinking they can't lose",
+    "Be nice to nerds. Chances are you'll end up working for one"
   ];
+
+  bio: string = "Bill Gates is an American business magnate, software developer, philanthropist, and author. He is best known as the co-founder of Microsoft Corporation.";
 }
 
 bootstrapApplication(App);
