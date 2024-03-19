48f8a86950bf0b1759cbd35ab6db8191786c4a34
pakkkek <kpojlukkreativnyj@gmail.com>
2024-03-19 15:58:28 +0100

    LW_18.03.24 was done.

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { bootstrapApplication } from '@angular/platform-browser';
 import 'zone.js';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   template: `
-    <h1>Hello from {{ name }}!</h1>
-    <p>{{ monologue }}</p>
-    <h2>Photo Gallery</h2>
-    <div *ngFor="let photo of photos">
-      <img [src]="photo" alt="Photo">
+    <div>
+      <h2>Досвід роботи</h2>
+      <p>{{ startDate }} - {{ endDate }} {{ jobTitle }}</p>
+    </div>
+
+    <div>
+      <h2>Досвід роботи</h2>
+      <p>{{ startDate }} - {{ endDate }} {{ jobTitle | uppercase }}</p>
+    </div>
+
+    <div>
+      <h2 [ngStyle]="{'color': 'blue'}">Досвід роботи</h2>
+      <p [ngClass]="{'highlight': true}">{{ startDate }} - {{ endDate }} {{ jobTitle | uppercase }}</p>
     </div>
   `,
+  styleUrls: ['./global_styles.css']
 })
 export class App {
-  name = 'Angular';
-  monologue: string = `
-    To be, or not to be, that is the question:
-    Whether 'tis nobler in the mind to suffer
-    The slings and arrows of outrageous fortune,
-    Or to take arms against a sea of troubles
-    And, by opposing, end them.
-  `;
-  photos: string[] = [
-    'assets/images/car.jpg',
-    'assets/images/logo.jpg',
-    'assets/images/founder.jpg',
-  ];
+  startDate: number = 2022;
+  endDate: number = 2024;
+  jobTitle: string = 'ITStep';
 }
 
-bootstrapApplication(App);
+@NgModule({
+  imports:      [ BrowserModule, CommonModule, FormsModule ],
+  declarations: [ App ],
+  bootstrap:    [ App ]
+})
+export class AppModule {}
+
+bootstrapApplication(AppModule);
